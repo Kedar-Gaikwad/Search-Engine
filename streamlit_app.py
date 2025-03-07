affc4916656c6b441c5e02c8776277709d89d403
Kedar-Gaikwad <87849115+Kedar-Gaikwad@users.noreply.github.com>
2025-03-07 01:25:30 +0000

    g

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,11 @@ import numpy
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 # Cache data loading
-import pandas as pd
-import streamlit as st
-
-
-# Cache data loading
 @st.cache_data
 def load_data():
     url = 'https://mystoragekedar.blob.core.windows.net/dataset/train.csv?sp=racw&st=2025-03-06T21:16:27Z&se=2025-03-07T05:16:27Z&spr=https&sv=2022-11-02&sr=b&sig=2NDQLc9AaIOWwKPAKTQVNPH7E5clugqGk3qd7gIbUo4%3D'
 
-    df = pd.read_csv(url, chunksize=10000)
+    df = pd.read_csv(url)
     return df.dropna()
 
 # Load dataset
